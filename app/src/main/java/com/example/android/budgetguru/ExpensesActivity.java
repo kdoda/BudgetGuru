211322dde0a12415253263894914d326edaa688f
kdoda <dodaklevin@gmail.com>
2019-05-13 13:32:33 -0600

    Pushing the repo in a public one, First Commit

---
+++ app/src/main/java/com/example/android/budgetguru/ExpensesActivity.java
@@ -0,0 +1,69 @@
+package com.example.android.budgetguru;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+//TODO Make this main activity and all those fragments
+public class ExpensesActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_statistics:
+                    fragment = new StatisticsFragment();
+                    break;
+                case R.id.navigation_incomes:
+                    fragment = new IncomesFragment();
+                    break;
+                case R.id.navigation_expenses:
+                    fragment = new ExpensesFragment();
+                    break;
+                case R.id.navigation_goals:
+                    fragment = new GoalsFragment();
+                    break;
+                case R.id.navigation_settings:
+                    fragment = new SettingsFragment();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new StatisticsFragment());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
